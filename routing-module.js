798590f5016f98cc6ab5b16c2b35b04e8e0be555
saurabs@patriot1tech.com <saurabs@patriot1tech.com>
2022-06-18 16:53:26 -0700

    updated frontend repo

---
+++ routing-module.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Router.init("mainArea", [
+  new Page("#signin", "pages/signin.html"),
+  new Page("#signup", "pages/signup.html"),
+  new Page("#testpage", "pages/test.html"),
+  new Page("#home", "pages/home.html", "pages/js/home.js"),
+  new Page("#history", "pages/history.html", "pages/js/history.js"),
+  new Page("#leaderboard", "pages/leaderboard.html", "pages/js/leaderboard.js"),
+  new Page("#play", "pages/play.html", "pages/js/play.js"),
+  new Page("#settings", "pages/settings.html", "pages/js/settings.js"),
+  new Page("#shop", "pages/shop.html", "pages/js/shop.js"),
+]);
