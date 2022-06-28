c060aca63dd3598a5def63f130ceb49620594390
Edward <epfrndz@gmail.com>
2022-06-27 17:09:59 -0700

    updated routing module as advised by elmer

--- routing-module.js
+++ routing-module.js
@@ -1,10 +1,10 @@
 "use strict";
 
 Router.init("mainArea", [
-  new Page("#signin", "pages/signin.html", "pages/js/signin.js"),
-  new Page("#signup", "pages/signup.html", "pages/js/signup.js"),
+  new Page("#signin", "pages/signin.html"),
+  new Page("#signup", "pages/signup.html"),
   new Page("#testpage", "pages/test.html"),
-  new Page("#home", "pages/home.html", "pages/js/home.js"),
+  new Page("#home", "pages/home.html", "pages/js/home.js", "pages/css/home.css"),
   new Page("#history", "pages/history.html", "pages/js/history.js"),
   new Page("#leaderboard", "pages/leaderboard.html", "pages/js/leaderboard.js"),
   new Page("#play", "pages/play.html", "pages/js/play.js"),
