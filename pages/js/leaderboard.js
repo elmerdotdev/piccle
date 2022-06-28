e6ebdd50ab893355d5e9e8767412869e86eff6b2
Edward <epfrndz@gmail.com>
2022-06-27 21:31:54 -0700

    created table of users by score (see console)

---
+++ pages/js/leaderboard.js
@@ -0,0 +1,73 @@
+import { getAuth, 
+} from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js'
+
+import {
+    getFirestore, collection, 
+    query, orderBy, limit,    
+    doc, getDocs, setDoc,
+} from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js'
+
+// initialize firebase services
+const auth = getAuth();
+const db = getFirestore();
+const user = auth.currentUser;
+console.log(user)
+if (user === null) {
+    console.log("User is not signed in");
+    alert("You are not signed in");
+    window.location.hash = "signin";
+}
+
+// reference to collections
+const colRefUsers = collection(db, "users")
+const q = query(colRefUsers, orderBy("score", "desc"));
+
+class Ranking {
+
+    constructor (rank, name, score, curUser) {
+        this.rank = rank;
+        this.name = name;
+        this.score = score;
+        this.curUser = curUser;
+    }
+    
+};
+
+const leaderboardTable = [];
+
+getDocs(q)
+.then((snapshot) => {
+    snapshot.forEach((doc) => {
+        const userInfo = doc.data();
+        const userName = displayPublicName(userInfo.firstname, userInfo.lastname);
+        const curUserFlag = (user.email === userInfo.user_email) ? true : false;
+        leaderboardTable.push( new Ranking((leaderboardTable.length + 1), userName, userInfo.score, curUserFlag));
+    })
+    console.table(leaderboardTable);
+})
+.catch((err) => {
+    console.log(err.message);
+})
+
+function displayPublicName (firstname, lastname) {
+    if ( (firstname.length + lastname.length + 1) > 10 ) {
+        return firstname
+    } else {
+        if ( (lastname === "") ) {
+            return firstname
+        } else {
+            return firstname + " " + lastname
+        }
+    }
+};
+
+// const leaderboardTable = document.createElement('table');
+// leaderboardTable.style.width = "600px";
+// const lbTableHeader = leaderboardTable.createTHead();
+// const lbHeaderRow = lbTableHeader.insertRow();
+// lbHeaderRow.insertCell("Rank");
+// lbHeaderRow.insertCell("Name");
+// lbHeaderRow.insertCell("Total Points");
+
+// document.getElementById("mainArea").appendChild(leaderboardTable);
+
