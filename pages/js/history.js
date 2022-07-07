41605379dd411eafa936598e4bcc214a92a9c00e
Elmer <contact@elmer.dev>
2022-07-06 19:24:52 -0700

    History functionality

--- pages/js/history.js
+++ pages/js/history.js
@@ -6,7 +6,9 @@ import {
     query,
     where,
     orderBy,
-    getDocs
+    getDocs,
+    doc,
+    getDoc
 } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
 
 function init() {
@@ -28,7 +30,6 @@ function init() {
 
     // Get list of progress
     const getProgress = async (filter) => {
-        const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         let progressQuery = ""
 
         const progressRef = collection(db, "progress");
@@ -51,18 +52,43 @@ function init() {
         const snapshot = await getDocs(progressQuery)
         try {
             snapshot.forEach((doc) => {
-                const date = doc.data().date_started.toDate()
-                const fullDate = `${date.getDate()} ${MONTHS[date.getMonth()]} ${date.getFullYear()}`
-                renderDOM(fullDate, doc.data().resolved, doc.data().tries)
+                if (!datesAreOnSameDay(new Date(doc.data().date_started.seconds * 1000), new Date())) {
+                    const fullDate = timestampToDate(doc.data().date_started)
+                    renderDOM(fullDate, doc.data().resolved, doc.data().tries, doc.id, doc.data().word)
+                } else {
+                    if (doc.data().resolved) {
+                        const fullDate = timestampToDate(doc.data().date_started)
+                        renderDOM(fullDate, doc.data().resolved, doc.data().tries, doc.id, doc.data().word)
+                    }
+                }
             })
         } catch (error) {
-            alert(error)
+            console.log(error)
         }
     }
 
-    const renderDOM = (date, resolved, tries) => {
+    // Convert timestamp to date
+    const timestampToDate = (timestamp) => {
+        const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+        let date = timestamp.toDate()
+        date = `${date.getDate()} ${MONTHS[date.getMonth()]} ${date.getFullYear()}`
+
+        return date
+    }
+
+    const datesAreOnSameDay = (first, second) => {
+        if ( first.getFullYear() === second.getFullYear() && first.getMonth() === second.getMonth() && first.getDate() === second.getDate()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Render progress history into DOM
+    const renderDOM = (date, resolved, tries, id, word) => {
         let message = ""
-        let element = ""
+        let boxElement = ""
         let elementClass = ""
 
         if (resolved) {
@@ -73,15 +99,57 @@ function init() {
             elementClass = "status-lose"
         }
 
-        element += `<div class="box ${elementClass}">` 
-        element += `<h3>${date}</h3>`
-        element += `<p>${message}</p>`
-        element += `<div>Hints used: ${tries}</div>`
-        element += `</div>`
+        boxElement += `<div class="box box-${id} ${elementClass}">`
+        boxElement += `<h3>${date}</h3>`
+        boxElement += `<p>${message}</p>`
+        boxElement += `<div>Hints used: ${tries}</div>`
+        boxElement += `</div>`
 
-        document.querySelector('.history-wrapper__body').innerHTML += element
+        document.querySelector('.history-wrapper__body').innerHTML += boxElement
+        setTimeout(() => {
+            document.querySelector('.box-' + id).addEventListener('click', () => { showPopup(date, message, resolved, tries, word) })
+        }, 100)
     }
 
+    // Get word info
+    const wordAndPoints = async (wordId) => {
+        const wordRef = doc(db, "words", wordId);
+        const snapshot = await getDoc(wordRef);
+
+        const wordPoints = {
+            word: snapshot.data().name,
+            points: snapshot.data().points
+        }
+
+        return wordPoints
+    }
+
+    // Show popup
+    const showPopup = async (date, message, resolved, tries, word) => {
+        let popupElement = ""
+        const wordpoints = await wordAndPoints(word)
+
+        document.querySelector('.history-wrapper__popup-overlay').classList.add('show')
+
+        popupElement += `<h3>${date}</h3>`
+        popupElement += `<p>${message}</p>`
+        popupElement += `<table>`
+        popupElement += `<tr><th>Word of the day:</th><td>${wordpoints.word}</td></tr>`
+        if (resolved) {
+            popupElement += `<tr><th>Points earned:</th><td>${wordpoints.points} Piccles</td></tr>`
+        }
+        popupElement += `<tr><th>Hints used:</th><td>${tries} Hints</td></tr>`
+        popupElement += `</table>`
+
+        document.querySelector('.popup-content').innerHTML = popupElement
+    }
+
+    // Close popup
+    document.querySelector('.history-wrapper__popup-overlay .close-btn').addEventListener('click', () => {
+        document.querySelector('.history-wrapper__popup-overlay').classList.remove('show')
+    })
+
+    // Monitor change of select dropdown
     document.querySelector('.filter').addEventListener('change', (e) => {
         document.querySelector('.history-wrapper__body').innerHTML = ""
         if (e.target.value == "win") {
@@ -89,11 +157,12 @@ function init() {
         } else if (e.target.value == "lose") {
             getProgress(false)
         } else {
-            getProgress(undefined)
+            getProgress()
         }
     })
 
     getProgress()
 
 }
+
 init()
