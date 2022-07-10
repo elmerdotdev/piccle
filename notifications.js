954b4d4e59f8c99f2b70bc1d75bace3e494d1db6
Elmer <contact@elmer.dev>
2022-07-09 22:44:59 -0700

    push notifications

---
+++ notifications.js
@@ -0,0 +1,25 @@
+const runPushNotifications = async () => {
+    const reg = await navigator.serviceWorker.getRegistration();
+    Notification.requestPermission().then(permission => {
+        if (permission !== 'granted') {
+            alert('you need to allow push notifications');
+        } else {
+            const timestamp = new Date().getTime() + 10 * 1000; // now plus 5000ms
+            reg.showNotification(
+                'Demo Push Notification',
+                {
+                    tag: timestamp, // a unique ID
+                    body: 'Hello World', // content of the push notification
+                    showTrigger: new TimestampTrigger(timestamp), // set the time for the push notification
+                    data: {
+                        url: window.location.href, // pass the current url to the notification
+                    },
+                    badge: './assets/badge.png',
+                    icon: './assets/icon.png',
+                }
+            );
+        }
+    });
+};
+
+runPushNotifications()
